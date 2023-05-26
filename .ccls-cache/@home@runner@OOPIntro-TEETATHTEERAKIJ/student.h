50605a88db945e2233c81b3ebb7b3117ab23eae2
ingT6481221 <132637600+ingT6481221@users.noreply.github.com>
2023-05-26 05:06:10 +0000

    Initial commit

---
+++ .ccls-cache/@home@runner@OOPIntro-TEETATHTEERAKIJ/student.h
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+class student{
+private:
+  long id;
+  string name;
+  int age;
+
+public:
+  void set_name(string);
+  void print_name();
+  void set_age(int);
+  int get_age();
+
+};
+
+void student::set_name(string n){ name=n;}
+void student::print_name(){
+  cout<< "Name: "<<name<<endl;
+}
+
+void student::set_age(int a) { age=a; }
+int student::get_age(){
+  return age;
+}
